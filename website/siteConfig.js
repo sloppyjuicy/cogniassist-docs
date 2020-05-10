3ad25fef9f631ae3665b6310fcbd1f186f693ac4
Alfred Francis <alfredfranciz@gmail.com>
2020-05-10 15:34:06 +0530

    intial commit

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Cogniassist Docs',
+  projectName: 'cogniassist-docs',
   organizationName: 'cogniwide',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
