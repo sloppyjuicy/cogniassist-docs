3e5fa278be5685ec955f0a8143c1d93ec0d1dbf5
Alfred Francis <alfredfranciz@gmail.com>
2020-05-16 22:25:58 +0530

    renamed docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'overview', label: 'Docs'},
+    {doc: 'introduction-to-cogniassist', label: 'Docs'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
